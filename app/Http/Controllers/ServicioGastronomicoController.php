9b58b1b72f27e53220d5736baedd6827d651fcbc
Monserrat <monseyalex4@gmail.com>
2026-07-13 14:39:15 -0600

    Fix: add ServicioGastronomicoController stub and correct contratos route in user menu

---
+++ app/Http/Controllers/ServicioGastronomicoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ServicioGastronomicoController extends Controller
+{
+    public function index()
+    {
+        return response('', 204);
+    }
+
+    public function create()
+    {
+        return response('', 204);
+    }
+
+    public function store(Request $request)
+    {
+        return response('', 204);
+    }
+
+    public function show($id)
+    {
+        return response('', 204);
+    }
+
+    public function edit($id)
+    {
+        return response('', 204);
+    }
+
+    public function update(Request $request, $id)
+    {
+        return response('', 204);
+    }
+
+    public function destroy($id)
+    {
+        return response('', 204);
+    }
+}
